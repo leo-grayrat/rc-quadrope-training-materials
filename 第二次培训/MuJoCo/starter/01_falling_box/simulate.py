04ce1afdc741d2b6f3a441478ec599b818296624
leo_grayrat <leo_grayrat@stu.xjtu.edu.cn>
2026-09-25 22:44:55 +0800

    keep first MuJoCo simulation example minimal

--- 第二次培训/MuJoCo/starter/01_falling_box/simulate.py
+++ 第二次培训/MuJoCo/starter/01_falling_box/simulate.py
@@ -1,5 +1,3 @@
-import time
-
 import mujoco
 import mujoco.viewer
 
@@ -9,11 +7,5 @@ data = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
     while viewer.is_running():
-        step_start = time.time()
-
         mujoco.mj_step(model, data)
         viewer.sync()
-
-        time_left = model.opt.timestep - (time.time() - step_start)
-        if time_left > 0:
-            time.sleep(time_left)
